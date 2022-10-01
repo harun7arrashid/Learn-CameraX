6613c5a37c2c7d3b320929eb3e5565e299daefa7
Harun Arrashid <harun@technopartner.co.id>
2022-10-01 19:35:36 +0700

    First Commit

---
+++ app/src/main/java/id/ackerman/learncamerax/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.ackerman.learncamerax
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
